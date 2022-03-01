d6f06495aa69ea0fd2352f8bcc514b81cc9aecda
harrisongers <hjgerstenlauer@gmail.com>
2022-03-01 17:53:30 -0500

    remove unnecessary variable

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Outlet } from "react-router";
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
-  const URL = "https://harrisongerst.herokuapp.com/";
+
 
   return (
     <div className="App">
